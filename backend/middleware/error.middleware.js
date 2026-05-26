24cfbf33d6526f3db5fbb3edc5a11a69c9853732
digitalrehman <abdulrehmanarain1018@gmail.com>
2026-05-26 12:16:06 +0500

    feat: add environment configuration and implement global error handling middleware

--- backend/middleware/error.middleware.js
+++ backend/middleware/error.middleware.js
@@ -5,7 +5,9 @@ export const errorHandler = (err, req, res, next) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || "Internal Server Error";
   if (err.code === 11000) {
-    message = "Duplicate field value entered (e.g. email already exists)";
+    const field = Object.keys(err.keyValue || {})[0] || "field";
+    const value = Object.values(err.keyValue || {})[0] || "";
+    message = `Duplicate field value entered: ${field} '${value}' already exists.`;
     statusCode = 400;
   }
 
